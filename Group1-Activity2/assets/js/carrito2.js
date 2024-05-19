ec035161d73c36212871fe6a61aceefee6c567bd
Milena Verenice Maldonado Córdova <mvmaldonado3@espe.edu.ec>
2024-05-18 22:42:51 -0500

    Cambios del carrito y del front

--- Group1-Activity2/assets/js/carrito2.js
+++ Group1-Activity2/assets/js/carrito2.js
@@ -1,5 +1,5 @@
 document.getElementById('buy-button').addEventListener('click', function() {
-    alert("Viva Cristo Rey! 2 uwu");
+
     const productTitle = document.querySelector('[name="product-title"]').value;
     const productPrice = document.querySelector('[name="product-price"]').value;
     const productImage = document.querySelector('[name="product-image"]').value;
@@ -19,11 +19,11 @@ document.getElementById('buy-button').addEventListener('click', function() {
 });
 
 document.getElementById('add-to-cart-button').addEventListener('click', function() {
-    alert("VIVA CRISTO REY!");
+    
     const productTitle = document.querySelector('[name="product-title"]').value;
     const productPrice = document.querySelector('[name="product-price"]').value;
     const productImage = document.querySelector('[name="product-image"]').value;
-    const productSize = document.querySelector('[name="product-size"]').value;
+    
 
     
     const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
@@ -32,8 +32,6 @@ document.getElementById('add-to-cart-button').addEventListener('click', function
         title: productTitle,
         price: parseFloat(productPrice),
         image: productImage,
-        size: productSize,
-        quantity: parseInt(productQuantity)
     });
     
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
